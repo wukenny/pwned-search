79e6337c3f9bb8cc552831865c35d26f55e21526
wukenny <wu.kenny@gmail.com>
2021-05-21 18:33:11 -0400

    Revert "Update pwned.py"
    This reverts commit 496d6332fb4c5fe43018efe3143b35ac08680132.

--- pwned.py
+++ pwned.py
@@ -33,10 +33,8 @@ def lookup_pwned_api(pwd):
         raise RuntimeError('Error fetching "{}": {}'.format(
             url, res.status_code))
     hashes = (line.split(':') for line in res.text.splitlines())
-    counts = {h:count for h, count in hashes}
-    return sha1pwd, counts.get(tail,0)
-    #count = next((int(count) for t, count in hashes if t == tail), 0)
-    #return sha1pwd, count
+    count = next((int(count) for t, count in hashes if t == tail), 0)
+    return sha1pwd, count
 
 
 def main(args):
